8c66eed5948007368b1731739f3cfd5d5813dc1b
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2024-09-23 11:55:11 +0200

    Début du projet, création du server

---
+++ api/server.js
@@ -0,0 +1,20 @@
+require ('dotenv').config()
+const express = require('express')
+const cors =  require('cors')
+const mongoose = require('mongoose')
+const port = process.env.PORT || 5000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('MongoDB connecté'))
+    .catch(err => console.error(err));
+
+    
+
+app.listen(port, ()=>{
+    console.log('Server lancé !!!!');
+    
+})
