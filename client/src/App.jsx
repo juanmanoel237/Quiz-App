83f63895ff49ffa6d8e130785bba97fed8410da4
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2024-09-24 16:45:15 +0200

    Mise à jour du composant App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,59 @@
-import './App.css';
-import { Routes, Route, Navigate } from "react-router-dom"
-import Quiz from "./Components/Quiz"
-import { useState } from 'react';
-import Navbar from './Components/Navbar';
-import Login from './Components/Login';
-import Register from './Components/Register';
-import Admin from './Components/Admin';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Quiz from "./Components/Quiz";
+import { useState } from "react";
+import Navbar from "./Components/Navbar";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import Admin from "./Components/Admin";
+import PrivateRoutes from "./Components/PrivateRoutes";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
 
-  const [isAdmin, setIsAdmin] = useState(false)
-  const [token, setToken] = useState(localStorage.getItem('token') || null)
+  const handleLogin = (token, adminStatus) => {
+    localStorage.setItem("token", token);
+    setIsAuthenticated(true);
+    setIsAdmin(adminStatus);
+  };
 
-  const handleLogin = (token, isAdmin) =>{
-    localStorage.setItem('token', token)
-    setToken(token)
-    setIsAdmin(isAdmin)
-  }
-
-  const handleLogout = ()=>{
-    localStorage.removeItem(token)
-    setToken(null)
-    setIsAdmin(false)
-  }
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsAuthenticated(false);
+    setIsAdmin(false);
+  };
 
   return (
     <div>
-      <Navbar isAdmin={isAdmin} handleLogout={handleLogout}/>
-      <Routes>
-        <Route path='/login' element={<Login handleLogin={handleLogin}/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/quiz' element={token ? <Quiz/> : <Navigate to="/login" />}/>
-        <Route path='/admin' element={isAdmin ? <Admin/> : <Navigate to="/login"/>}/>
-        <Route path='/' element={<Navigate to="/quiz"/>}/>
-      </Routes>
+      <Router>
+        <Navbar
+          isAdmin={isAdmin}
+          handleLogout={handleLogout}
+          isAuthenticated={isAuthenticated}
+        />
+        <Switch>
+          <Route path="/login">
+            <Login handleLogin={handleLogin} />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <PrivateRoutes
+            path="/quiz"
+            component={Quiz}
+            isAuthenticated={isAuthenticated}
+          />
+          <PrivateRoutes
+            path="/admin"
+            component={Admin}
+            isAuthenticated={isAdmin}
+          />
+          <Route path="/">
+            <h2>Bienvenue sur Quiz App!</h2>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
