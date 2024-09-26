d16fe850eb513afadeeab335c0a58cdcfc3309ce
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2024-09-26 16:50:21 +0200

    Début du menu Admin

--- client/src/Components/Admin.jsx
+++ client/src/Components/Admin.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "../Styles/Admin.css"
+import axios from 'axios'
 
 const Admin = () => {
+
+    const [questions, setQuestions] = useState([])
+    const [newQuestion, setNewQuestions] = useState('')
+    const [newAnswers, setNewAnswers] = useState(['', '', '', '']);
+    const [correctAnswer, setCorrectAnswer] = useState('');
+
+    useEffect(()=>{
+        const fetchQuestions = async ()=>{
+            const res = await axios.get('http://localhost:5000/api/questions')
+            setQuestions(res.data)
+        }
+        fetchQuestions()
+    },[])
   return (
     <div>Admin</div>
   )
